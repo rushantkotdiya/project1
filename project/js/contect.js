1e7efd9445dc94044dc36d91d07a817fb011d184
rushantkotdiya <167356441+rushantkotdiya@users.noreply.github.com>
2025-04-18 09:25:47 +0530

    Delete project directory

--- project/js/contect.js
+++
@@ -1,6 +0,0 @@
-const form = document.getElementById('contactForm');
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
-  alert('Thank you for contacting Manganresto! We will get back to you soon.');
-  form.reset();
-});
